d16f2e375442c5696a108a7fe76cd1a816f0aab4
newhask777 <newhaskellisp@gmail.com>
2026-06-28 19:59:09 +0300

    init

---
+++ App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Button,
+  FlatList
+} from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  const removeGoalHandler =  goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId)
+    })
+  }
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            id={itemData.item.id}
+            title={itemData.item.value}
+            
+            onDelete={removeGoalHandler}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+});
